3d5bab088a9eb3f941cae7e87a4b4d4d491ee830
Matthew Cantelon <gh@matthewcantelon.ca>
2017-01-31 19:08:18 -0800

    Tidied tests and removed linux only reporters

--- tests/ApprovalTest.php
+++ tests/ApprovalTest.php
@@ -2,28 +2,25 @@
 
 use PHPUnit\Framework\TestCase;
 use ApprovalTests\Approvals;
-use ApprovalTests\Reporters\OpenDiffReporter;
 
 class ApprovalTest extends TestCase
 {
     public function testApproveArray()
     {
-        //Approvals::useReporter(new OpenDiffReporter());
-        $list = array('zero', 'one', 'two', 'three', 'four', 'five');
+        $list = ['zero', 'one', 'two', 'three', 'four', 'five'];
         Approvals::approveList($list);
     }
 
     public function testApproveMap()
     {
-        //Approvals::useReporter(new OpenDiffReporter());
-        $list = array(
+        $list = [
             'zero' => 'Lance',
             'one' => 'Jim',
             'two' => 'James',
             'three' => 'LLewellyn',
             'four' => 'Asaph',
             'five' => 'Dana'
-        );
+        ];
         Approvals::approveList($list);
     }
 
