c70c900d10b01cc5d48f681e146d920483ba3b58
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-02-24 10:03:24 -0700

    t update type of error
    Co-Authored-By: Josh Kelley <476338+joshkel@users.noreply.github.com>

--- tests/ErrorHandlerTest.php
+++ tests/ErrorHandlerTest.php
@@ -7,6 +7,6 @@ class ErrorHandlerTest extends TestCase
     public function testEWarning()
     {
         $this->expectException(\ErrorException::class);
-        trigger_error('Demo warning', E_WARNING);
+        trigger_error('Demo warning', E_USER_WARNING);
     }
 }
